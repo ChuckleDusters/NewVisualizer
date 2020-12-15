c0eb06ae535e526b2246f6a4b29b3a16ed737f52
ChuckleDusters <thatoneheros451@gmail.com>
2020-12-14 17:33:31 -0700

    Fixed infinite reload loop error

--- iPhone.js
+++
@@ -1,7 +0,0 @@
-let isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
-(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
-!window.MSStream
-
-if (isIOS === true) {
-    window.location.href = "iPhone.html";
-}
--- pageScripts.js
+++ pageScripts.js
@@ -1,3 +1,11 @@
+let isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
+(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
+!window.MSStream
+
+if (isIOS === true) {
+    window.location.href = "iPhone.html";
+}
+
 function initPage() {
     document.body.classList.remove('preload');
 }
